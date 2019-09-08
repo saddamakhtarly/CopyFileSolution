3c43e02fe03840afc3c70d20e0f0f4302c14aed4
Saddam Akhtar <saddamakhtar.ly@gmail.com>
2019-09-08 14:53:42 +0500

    Complete First Version
    Complete First Version
    
    Co-Authored-By: Saddam Akhtar <saddamakhtar.ly@gmail.com>

---
+++ CopyFileSolution/CopyFile/MyCopy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFile
+{
+    public class MyCopy
+    {
+        public string SourceAddress { get; set; }
+        public string DestinationAddress { get; set; }
+
+        public void Copy()
+        {
+            using (FileStream reader = new FileStream(SourceAddress, FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream writer = new FileStream(DestinationAddress, FileMode.Create, FileAccess.Write))
+                {
+                    byte[] block = new byte[1024 * 1024];
+                    long fileSize = reader.Length;
+                    double writtenSize = 0;
+                    int blockSize = 0;
+
+                    while ((blockSize = reader.Read(block, 0, block.Length)) > 0)
+                    {
+                        writtenSize += blockSize;
+                        int progress = Convert.ToInt32(writtenSize / fileSize * 100);
+                        writer.Write(block, 0, blockSize);
+                    }
+                }
+            }
+        }
+    }
+}
